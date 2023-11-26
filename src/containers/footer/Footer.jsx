16ee8810f8be1a141b37b7eb69295ebab7770be3
Vlad <sambipush@mail.ru>
2023-11-26 15:49:30 +0300

    done with footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,16 +1,20 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./footer.scss";
-import logo from '../../assets/img/logo.svg';
+import logo from "../../assets/img/logo.svg";
 
-function Footer({padding, switchPadding}) {
-	useEffect(() => {
+function Footer({ padding, switchPadding }) {
+    useEffect(() => {
         switchPadding();
         window.addEventListener("resize", switchPadding);
     }, []);
 
     return (
         <div className="gpt3__footer ">
-            <div className={`gpt3__footer-container container ${padding ? '' : 'switch-padding'}`}>
+            <div
+                className={`gpt3__footer-container container ${
+                    padding ? "" : "switch-padding"
+                }`}
+            >
                 <div className="gpt3__footer-container_heading">
                     <h1 className="gradient__text">
                         Do you want to step in to the future before others
@@ -20,47 +24,47 @@ function Footer({padding, switchPadding}) {
                     </button>
                 </div>
                 <div className="gpt3__footer-container_content">
-                    <div className="gpt3__footer-container_content-logo">
-                        <img src={logo} alt="logo" />
-                        <p>
-                            Crechterwoord K12 182 DK Alknjkcb, All Rights
-                            Reserved
-                        </p>
-                    </div>
-                    <div className="gpt3__footer-container_content-menu">
-                        <ul className="menu-list">
-                            <li className="menu-list__item">
-                                <h2>Links</h2>
-                                <div>
-                                    <a href="!#">Overons</a>
-                                    <a href="!#">Social Media</a>
-                                    <a href="!#">Counters</a>
-                                    <a href="!#">Contact</a>
-                                </div>
-                            </li>
-                            <li className="menu-list__item">
-                                <h2>Company</h2>
-                                <div>
-                                    <a href="!#">Terms & Conditions</a>
-                                    <a href="!#">Privacy Policy</a>
-                                    <a href="!#">Contact</a>
-                                </div>
-                            </li>
-                            <li className="menu-list__item">
-                                <h2>Get in touch</h2>
-                                <div>
-                                    <a href="!#">
-                                        Crechterwoord K12 182 DK Alknjkcb
-                                    </a>
-                                    <a href="!#">085-132567</a>
-                                    <a href="!#">info@payme.net</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                    <ul className="gpt3__footer-container_content-menu">
+                        <li className="menu-list__item">
+                            <div className="menu-list__item-logo">
+                                <img src={logo} alt="logo" />
+                                <p>
+                                    Crechterwoord K12 182 DK Alknjkcb, All
+                                    Rights Reserved
+                                </p>
+                            </div>
+                        </li>
+                        <li className="menu-list__item">
+                            <h2>Links</h2>
+                            <div className="menu-list__item-links">
+                                <a href="!#">Overons</a>
+                                <a href="!#">Social Media</a>
+                                <a href="!#">Counters</a>
+                                <a href="!#">Contact</a>
+                            </div>
+                        </li>
+                        <li className="menu-list__item">
+                            <h2>Company</h2>
+                            <div className="menu-list__item-links">
+                                <a href="!#">Terms & Conditions</a>
+                                <a href="!#">Privacy Policy</a>
+                                <a href="!#">Contact</a>
+                            </div>
+                        </li>
+                        <li className="menu-list__item">
+                            <h2>Get in touch</h2>
+                            <div className="menu-list__item-links">
+                                <a href="!#">
+                                    Crechterwoord K12 182 DK Alknjkcb
+                                </a>
+                                <a href="!#">085-132567</a>
+                                <a href="!#">info@payme.net</a>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
                 <div className="gpt3__footer-container_copyrights">
-					<p>© 2021 GPT-3. All rights reserved.</p>
+                    <p>© 2021 GPT-3. All rights reserved.</p>
                 </div>
             </div>
         </div>
